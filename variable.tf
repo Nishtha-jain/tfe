4cd30810d9b4522d9b66a48526697bb2861e4be5
Nishtha <jainnishtha401@gmail.com>
2024-01-11 07:57:34 +0000

    variable testing

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "example_var" {
+  type        = string
+  description = "An example variable"
+  default     = "default_value"
+}
+
+variable "another_var" {
+  type        = number
+  description = "Another example variable"
+  default     = 42
+}
+
