a4b1ae257f12f8d26cd1f851cced0b2ad74b3756
Nishtha <jainnishtha401@gmail.com>
2024-01-12 09:25:59 +0000

    checking local-exec

--- example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
- 
-export TF_VAR_region="us-east-1"
-export TF_VAR_instance_type="t2.micro"
--- instance.tf
+++ instance.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
- 
+
 resource "aws_instance" "example" {
   ami           = "ami-0005e0cfe09cc9050"
-  instance_type = var.instance_type
-}
- 
-provisioner "local-exec" {
-  command = "./example.sh && terraform apply -auto-approve"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo $(self.private_ip), $(self.public_ip) >> private_and_public_ips.txt"
+  }
 }
