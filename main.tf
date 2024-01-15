2e6fe4e05a90bc6edeeb9249be03a436cf9369da
Nishtha <jainnishtha401@gmail.com>
2024-01-15 06:48:12 +0000

    removinf variable block

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ provider "tfe" {
   token        = var.tfe_token
 }
 
-variable "tfe_token" {
-  description = "Terraform Enterprise API token"
-}
-
 resource "null_resource" "set_tfe_variable" {
   provisioner "local-exec" {
     command = "./set_tfe_variable.sh ${var.tfe_token}"
